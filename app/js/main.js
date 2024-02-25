626fc690e2a6b8ea6977d504db7908616bd6e452
Dolbezhnik <krendelalenkin@gmail.com>
2024-02-25 18:58:13 +0300

    Section customers is donegit add -A!

--- app/js/main.js
+++ app/js/main.js
@@ -82,4 +82,23 @@ window.addEventListener('DOMContentLoaded', ()=>{
         });
       
     });
+    //jquery owl carousel
+    $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:30,
+      nav:false,
+      dots:true,
+      stagePadding: 0,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:2
+          },
+          1000:{
+              items:2
+          }
+      }
+  })
 })
